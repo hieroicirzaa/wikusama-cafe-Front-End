a273ff248198da45ab045a044a6f65679d9843a2
hieroicirzaa <nationirza@gmail.com>
2023-10-07 23:53:29 +0700

    mengubah tampilan laporan menggunakan antd

--- src/pages/manajer/Laporan.jsx
+++ src/pages/manajer/Laporan.jsx
@@ -1,36 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-import { useNavigate } from "react-router-dom";
+import { DatePicker, Space } from 'antd';
+import { useNavigate } from 'react-router-dom';
 import { baseURL, config } from '../../config';
 
+const { RangePicker } = DatePicker;
+
 function Laporan() {
-  const [tanggal, setTanggal] = useState('');
-  const [bulan, setBulan] = useState('');
-  const [tahun, setTahun] = useState('');
+  const [dateRange, setDateRange] = useState(null);
+  const [monthRange, setMonthRange] = useState(null);
+  const [yearRange, setYearRange] = useState(null);
   const [tampilData, setTampilData] = useState([]);
   const [noTransaksi, setNoTransaksi] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+
   useEffect(() => {
     if (localStorage.getItem("users") !== `"manajer"`) {
-      navigate("/");
-    }
+      navigate("/"); 
+    } 
   }, []);
 
-  const handleFormSubmitTanggal = async (event) => {
-    event.preventDefault();
-
+  const handleFormSubmitTanggal = async () => {
     try {
-      setLoading(true); // Menandakan bahwa permintaan sedang berlangsung
-      setError(null); // Menghapus pesan kesalahan sebelumnya (jika ada)
+      setLoading(true);
+      setError(null);
 
-      let date = `${tanggal}`;
+      const startDate = dateRange[0].format('YYYY-MM-DD');
+      const endDate = dateRange[1].format('YYYY-MM-DD');
 
       const response = await axios.post(
-        baseURL + "/transaksi/jumlahPendapatanTanggal",
-        { date: date },
+        baseURL + '/transaksi/jumlahPendapatanTanggal',
+        { startDate, endDate },
         config
       );
 
@@ -38,26 +40,24 @@ function Laporan() {
       setTampilData(data);
       setNoTransaksi(data.length === 0);
     } catch (error) {
-      setError("Terjadi kesalahan saat memuat data."); // Menampilkan pesan kesalahan kepada pengguna
+      setError('Terjadi kesalahan saat memuat data: ' + error.message);
       console.error(error);
     } finally {
-      setLoading(false); // Menandakan bahwa permintaan telah selesai
+      setLoading(false);
     }
   };
 
-  const handleFormSubmitBulan = async (event) => {
-    event.preventDefault();
-
+  const handleFormSubmitBulan = async () => {
     try {
-      setLoading(true); // Menandakan bahwa permintaan sedang berlangsung
-      setError(null); // Menghapus pesan kesalahan sebelumnya (jika ada)
+      setLoading(true);
+      setError(null);
 
-      let month = `${bulan}`;
-      let year = `${tahun}`;
+      const startMonth = monthRange[0].format('YYYY-MM');
+      const endMonth = monthRange[1].format('YYYY-MM');
 
       const response = await axios.post(
-        baseURL + "/transaksi/jumlahPendapatanBulanan",
-        { month: month, year: year },
+        baseURL + '/transaksi/jumlahPendapatanBulanan',
+        { startMonth, endMonth },
         config
       );
 
@@ -65,24 +65,24 @@ function Laporan() {
       setTampilData(data);
       setNoTransaksi(data.length === 0);
     } catch (error) {
-      setError("Terjadi kesalahan saat memuat data."); // Menampilkan pesan kesalahan kepada pengguna
+      setError('Terjadi kesalahan saat memuat data: ' + error.message);
       console.error(error);
     } finally {
-      setLoading(false); // Menandakan bahwa permintaan telah selesai
+      setLoading(false);
     }
   };
 
-  const handleFormSubmitTahun = async (event) => {
-    event.preventDefault();
-
+  const handleFormSubmitTahun = async () => {
     try {
-      setLoading(true); // Menandakan bahwa permintaan sedang berlangsung
-      setError(null); // Menghapus pesan kesalahan sebelumnya (jika ada)
-      let year = `${tahun}`;
+      setLoading(true);
+      setError(null);
+
+      const startYear = yearRange[0].format('YYYY');
+      const endYear = yearRange[1].format('YYYY');
 
       const response = await axios.post(
-        baseURL + "/transaksi/jumlahPendapatanTahunan",
-        { year: year },
+        baseURL + '/transaksi/jumlahPendapatanTahunan',
+        { startYear, endYear },
         config
       );
 
@@ -90,97 +90,64 @@ function Laporan() {
       setTampilData(data);
       setNoTransaksi(data.length === 0);
     } catch (error) {
-      setError("Terjadi kesalahan saat memuat data."); // Menampilkan pesan kesalahan kepada pengguna
+      setError('Terjadi kesalahan saat memuat data: ' + error.message);
       console.error(error);
     } finally {
-      setLoading(false); // Menandakan bahwa permintaan telah selesai
+      setLoading(false);
     }
   };
 
   return (
-    <div className="flex pr-4  ml-6 mt-4 ">
+    <div className="flex pr-4 ml-6 mt-4">
       <div className="w-full h-screen">
-        {/* berdasarkan tanggal */}
         <h1 className="text-2xl font-bold mb-4">Tampil Jumlah Pendapatan Berdasarkan Tanggal</h1>
-        <form onSubmit={handleFormSubmitTanggal} className="mb-4">
-
-          <div className="flex mb-2">
-            <label className="mr-2">Tanggal:</label>
-            <input
-              type="date"
-              value={tanggal}
-              onChange={(e) => setTanggal(e.target.value)}
-              className="border border-gray-300 px-2 py-1 rounded" required
-            />
-          </div>
-          <button className="btn bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+        <Space direction="vertical" size={12}>
+          <RangePicker value={dateRange} onChange={(dates) => setDateRange(dates)} />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" onClick={handleFormSubmitTanggal}
+          >
             Tampilkan
           </button>
-        </form>
+        </Space>
 
-        {/* berdasarkan bulan dan tahun */}
-        <h1 className="text-2xl font-bold mb-4">Tampil Jumlah Pendapatan Berdasarkan Bulan Tertentu & Pada Tahun Tertentu</h1>
-        <form onSubmit={handleFormSubmitBulan} className="mb-4">
-          <div className="flex mb-2">
-            <label className="mr-2">Bulan:</label>
-            <select
-              value={bulan}
-              onChange={(e) => setBulan(e.target.value)}
-              className="border border-gray-300 px-2 py-1 rounded" required
-            >
-              <option value="">Pilih Bulan</option>
-              <option value="1">Januari</option>
-              <option value="2">Februari</option>
-              <option value="3">Maret</option>
-              <option value="4">April</option>
-              <option value="5">Mei</option>
-              <option value="6">Juni</option>
-              <option value="7">Juli</option>
-              <option value="8">Agustus</option>
-              <option value="9">September</option>
-              <option value="10">Oktober</option>
-              <option value="11">November</option>
-              <option value="12">Desember</option>
-            </select>
-          </div>
-          <div className="flex mb-2">
-            <label className="mr-2">Tahun:</label>
-            <input
-              type="number"
-              value={tahun}
-              onChange={(e) => setTahun(e.target.value)}
-              className="border border-gray-300 px-2 py-1 rounded" required
-            />
-          </div>
-          <button className="btn bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+        <h1 className="text-2xl font-bold my-4">Tampil Jumlah Pendapatan Berdasarkan Rentang Bulan</h1>
+        <Space direction="vertical" size={12}>
+          <RangePicker
+            picker="month"
+            value={monthRange}
+            onChange={(dates) => setMonthRange(dates)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" onClick={handleFormSubmitBulan}
+          >
             Tampilkan
           </button>
-        </form>
+        </Space>
 
-        {/* Berdasarkan Tahun */}
-        <h1 className="text-2xl font-bold mb-4">Tampil Jumlah Pendapatan Berdasarkan Tahun</h1>
-        <form onSubmit={handleFormSubmitTahun} className="mb-4">
-          <div className="flex mb-2">
-            <label className="mr-2">Tahun:</label>
-            <input
-              type="number"
-              value={tahun}
-              onChange={(e) => setTahun(e.target.value)}
-              className="border border-gray-300 px-2 py-1 rounded" required
-            />
-          </div>
-          <button className="btn bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+        <h1 className="text-2xl font-bold my-4">Tampil Jumlah Pendapatan Berdasarkan Rentang Tahun</h1>
+        <Space direction="vertical" size={12}>
+          <RangePicker
+            picker="year"
+            value={yearRange}
+            onChange={(dates) => setYearRange(dates)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" onClick={handleFormSubmitTahun}
+          >
             Tampilkan
           </button>
-        </form>
+        </Space>
 
-        <div>
+        <div className='mt-4'>
           {loading ? (
             <p>Loading...</p>
           ) : error ? (
             <p>{error}</p>
           ) : tampilData.length === 0 && !noTransaksi ? (
-            <p>Tidak ada pendapatan yang ditemukan pada tanggal, bulan dan tahun ini.</p>
+            <p>Tidak ada pendapatan yang ditemukan dalam rentang yang dipilih.</p>
           ) : (
             tampilData.length > 0 && (
               <table className="min-w-full divide-y divide-gray-300">
@@ -198,8 +165,9 @@ function Laporan() {
                   {tampilData.map((transaksiItem) => (
                     <tr key={transaksiItem.id}>
                       <td className="px-6 py-4 whitespace-nowrap">
-                        Rp{" "}
-                        {new Intl.NumberFormat("id-ID").format(transaksiItem.total_income)}</td>
+                        Rp{' '}
+                        {new Intl.NumberFormat('id-ID').format(transaksiItem.total_income)}
+                      </td>
                     </tr>
                   ))}
                 </tbody>
